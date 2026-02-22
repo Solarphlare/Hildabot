8fd199e4573c1c56786513629a633afa3a3c72da
William M. <cominatyou@cominatyou.com>
2026-02-22 00:41:42 -0600

    Fix comment typo

--- include/constants.h
+++ include/constants.h
@@ -11,7 +11,7 @@
 #ifndef DEBUG
 /// A channel within the guild specified by BASE_GUILD_ID for running bot commands.
 #define BOT_CHANNEL_ID 495034452422950915ULL
-/// A channel ithin the guild specified by BASE_GUILD_ID that facilitates a testing environment for the bot.
+/// A channel within the guild specified by BASE_GUILD_ID that facilitates a testing environment for the bot.
 #define TESTING_CHANNEL_ID 565327145786802176ULL
 /// The guild where the bot will be active.
 #define BASE_GUILD_ID 492572315138392064ULL
